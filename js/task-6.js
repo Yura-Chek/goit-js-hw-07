30aaaa6f0c49bbd0e9534004415a115365e2be59
Chekan <ky.chekan@gmail.com>
2025-02-16 14:41:37 +0200

    task-6 middle

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const box = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", () => {
+    const count = Number(input.value);
+    
+    
+    box.innerHTML = "";
+}
